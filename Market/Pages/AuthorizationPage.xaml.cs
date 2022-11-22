82b0ea5c93b0a37560997df682069c864f0550dd
Amenisk <k.ziyatdinov2003@gmail.com>
2022-11-22 18:34:55 +0300

    Добавьте файлы проекта.

---
+++ Market/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Market.ADO;
+
+namespace Market320.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+        private void RegistrationNavigate(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegistrationPage());
+        }
+
+        private void Authorization(object sender, RoutedEventArgs e)
+        {
+            MarketEntities db = new MarketEntities();
+            if (tbLogin.Text != "" && tbPassword.Text != "")
+            {
+                var account = db.Account.FirstOrDefault(x => x.Login == tbLogin.Text && x.Password == tbPassword.Text);
+                if (account != null)
+                {
+                    if(account.User.Role.Name == "Admin")
+                    {
+                        NavigationService.Navigate(new AdminPage());
+                    }
+                    else
+                    {
+                        NavigationService.Navigate(new BuyerPage(account));                
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Not have user with written login!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fill login and password please!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Market/Pages/BuyerPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Market.ADO;
+
+namespace Market320.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для BuyerPage.xaml
+    /// </summary>
+    public partial class BuyerPage : Page
+    {
+        private Account _acc;
+        private List<Basket> _basket = new List<Basket>();
+        private List<Product> _products = new List<Product>();
+        public BuyerPage(Account acc)
+        {
+            InitializeComponent();
+            ProductList.ItemsSource = App.Connection.Product.ToList();
+            _acc = acc;
+            _basket = App.Connection.Basket.Where(x => x.User_Id == _acc.User_Id).ToList();
+            foreach (var prod in _basket)
+            {
+                _products.Add(App.Connection.Product.Where(x => x.Product_Id == prod.Product_Id).FirstOrDefault());
+            }
+            BasketList.ItemsSource = _products;
+        }
+
+        private void Select(object sender, SelectionChangedEventArgs e)
+        {
+            var product = ProductList.SelectedItem as Product;
+
+            var newBasket = new Basket
+            {
+                User_Id = _acc.User_Id,
+                Product_Id = product.Product_Id
+            };
+            App.Connection.Basket.Add(newBasket);
+            App.Connection.SaveChanges();
+
+            _products.Add(product);
+            BasketList.Items.Refresh();
+        }
+    }
+}
